6bdf84e1883c9b8693c8cb8563da79d8fef16378
Uday Aggarwal <udayaggarwal1808@gmail.com>
2026-07-17 06:58:57 +0530

    Implement client-side validation and server-side error handling

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust"
 const path = require("path");
 const ejsMate=require("ejs-mate");
 const methodOverride=require("method-override");
+const wrapAsync=require("./utils/wrapAsync");
+const ExpressError=require("./utils/ExpressError");
+const listingSchema=require("./schema.js");
 app.engine("ejs", ejsMate);
 app.use(methodOverride("_method"));
 main().
@@ -27,6 +30,21 @@ app.use(express.urlencoded({extended:true}));
 app.listen(8080, ()=>{
     console.log("Server is listening");
 })
+
+
+//Middleware for validating the listing data using joi
+const validateListing=(req,res,next)=>{
+    let result=listingSchema.validate(req.body);
+
+    if(result.error){
+        let msg=result.error.details.map((el)=>el.message).join(",");
+        next(new ExpressError(400,msg)); 
+    } else {
+        next();
+    }
+};
+
+
 app.get("/testlisting", async (req,res)=>{
     let samplelisting=new Listing({
         title: "My new Villa",
@@ -38,40 +56,58 @@ app.get("/testlisting", async (req,res)=>{
     await samplelisting.save()
     res.send("It is working");
 })
+
 app.get("/listings", async (req,res)=>{
     let alllisting=await Listing.find({});
     res.render("listings/index.ejs",{alllisting});
 });
+
+
 app.get("/listings/new", (req,res)=>{
     res.render("listings/new.ejs");
 })
-app.get("/listings/:id", async(req,res)=>{
+//Show Route
+app.get("/listings/:id", wrapAsync(async (req,res)=>{
     let {id}=req.params;
     const list= await Listing.findById(id);
     res.render("listings/show.ejs",{list});
-})
+}))
 app.get("/", (req,res)=>{
     res.send("Welcome to Wanderlust");
 })
-app.post("/listings/new", async (req,res)=>{
+//New page for creating new listing
+app.post("/listings",validateListing, wrapAsync(async (req,res,next)=>{
     let {listing} =req.body;
     const newlis=new Listing(req.body.listing);
     await newlis.save()
     res.redirect("/listings");
-})
-app.get("/listings/:id/edit", async (req,res)=>{
+}))
+//Edit and Update Route
+app.get("/listings/:id/edit", wrapAsync(async (req,res)=>{
     let {id}=req.params;
     let list=await Listing.findById(id);
     res.render("listings/edit.ejs",{list});
-})
-app.put("/listings/:id", async(req,res)=>{
+}))
+//Update Route
+app.put("/listings/:id", validateListing, wrapAsync(async(req,res)=>{
     let {id}=req.params;
     await Listing.findByIdAndUpdate(id,{...req.body.listing});
     res.redirect("/listings");
-})
-app.delete("/listings/:id", async(req,res)=>{
+}))
+//Delete Route
+app.delete("/listings/:id", wrapAsync(async(req,res)=>{
     let {id}=req.params;
     let deleted=await Listing.findByIdAndDelete(id);
     console.log(deleted);
     res.redirect("/listings");
+}))
+//Error Handling
+app.all("/*splat", (req, res, next) => {
+    next(new ExpressError(404, "Page Not Found"));
+});
+//Error Handling Middleware
+app.use((err,req,res,next)=>{
+    let {statusCode=500, message="Something went wrong"}=err;
+    // res.status(statusCode).send(message);
+    res.render("listings/errors.ejs",{err});
 })
--- init/inti.js
+++ init/inti.js
@@ -4,7 +4,7 @@ const Listing=require("../models/listing.js");
 const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust"
 main().
 then(()=>{
-    console.log("Connected to DB");
+    console.log("Connected to DB"); 
 })
 .catch((err) => {
     console.log(err)
